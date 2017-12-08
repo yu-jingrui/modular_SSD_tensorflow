eda21b8adbb91225878b60276c57af000cafac13
Jingrui Yu <fisheess@gmail.com>
2017-12-08 11:55:00 +0100

    define eval_dir in eval_params

--- evaluator/eval_params.py
+++ evaluator/eval_params.py
@@ -4,6 +4,7 @@ from collections import namedtuple
 EvaluatorParams = namedtuple(
     'EvaluatorParameters',
     ['checkpoint_path',  # directory must have '/' at the end
+     'eval_dir',  # directory to save evaluation results
      'use_finetune',  # whether use checkpoints under 'finetune/' folder
      'is_training',  # whether evaluate while training is ongoing
      'eval_train_dataset',  # whether evaluate against training dataset
@@ -14,6 +15,7 @@ EvaluatorParams = namedtuple(
 
 eval_while_training = EvaluatorParams(
     checkpoint_path='./logs/',
+    eval_dir='./logs/eval',
     use_finetune=False,
     is_training=True,
     eval_train_dataset=False,
@@ -23,6 +25,7 @@ eval_while_training = EvaluatorParams(
 
 eval_while_finetuning = EvaluatorParams(
     checkpoint_path='./logs/',
+    eval_dir='./logs/eval',
     use_finetune=True,
     is_training=True,
     eval_train_dataset=False,
@@ -32,6 +35,7 @@ eval_while_finetuning = EvaluatorParams(
 
 eval_only_last_ckpt = EvaluatorParams(
     checkpoint_path='./logs/',
+    eval_dir='./logs/eval',
     use_finetune=False,
     is_training=False,
     eval_train_dataset=False,
@@ -41,6 +45,7 @@ eval_only_last_ckpt = EvaluatorParams(
 
 eval_only_last_ckpt_finetune = EvaluatorParams(
     checkpoint_path='./logs/',
+    eval_dir='./logs/eval',
     use_finetune=True,
     is_training=False,
     eval_train_dataset=False,
@@ -50,6 +55,7 @@ eval_only_last_ckpt_finetune = EvaluatorParams(
 
 debug_params = EvaluatorParams(
     checkpoint_path='/home/yjin/SSD/tmp/logs/',
+    eval_dir='./logs/eval',
     use_finetune=False,
     is_training=False,
     eval_train_dataset=True,
